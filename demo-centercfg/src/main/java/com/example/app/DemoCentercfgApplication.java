2005b578c562bc16f370941e2b6847bb495213e0
streammoon <streammoon@126.com>
2020-05-01 18:54:05 +0800

    V-0.4 -- T01-V0.1

--- demo-centercfg/src/main/java/com/example/app/DemoCentercfgApplication.java
+++ demo-centercfg/src/main/java/com/example/app/DemoCentercfgApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * 启动类
+ * 启动类---分之T01-V01
  */
 @SpringBootApplication
 @EnableConfigServer
