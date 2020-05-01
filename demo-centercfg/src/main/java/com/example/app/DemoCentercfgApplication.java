41aec66af990104f38e869af140190d15c442463
streammoon <streammoon@126.com>
2020-05-01 18:34:03 +0800

    V-0.2

--- demo-centercfg/src/main/java/com/example/app/DemoCentercfgApplication.java
+++ demo-centercfg/src/main/java/com/example/app/DemoCentercfgApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
